16241ce98bb8ff781771301d240a5e2af25ef1cd
Agustin Sch <84196634+AgusTony8@users.noreply.github.com>
2024-03-12 17:27:05 -0300

    change gitignore to keep dist and src modules

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import './style.css';
+import CoffeBg from  './tulipt_coffee.jpg'
+import html from "./index.html";
+
+
+ function alerting() {
+    alert("Hello, TONY! This is your alert statement.");
+
+ }
+
+//document.addEventListener('DOMContentLoaded',alerting())
